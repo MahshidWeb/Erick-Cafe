3f9408927470d27e66c3a7126ab1b761d7b74e96
Mahshid <mahshidmonirii1@gmail.com>
2025-10-15 17:30:17 +0330

    Sidebar with JS

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
- 
+ const btnside=document.querySelector('.berger-menu');
+ const sidebar=document.querySelector('.sidebar');
+ const containermen=document.querySelector('.container-menu2');
+
+ btnside.addEventListener('click',function(){
+    sidebar.classList.toggle('show');
+    containermen.classList.toggle('showul');
+ })
